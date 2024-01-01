b1aad9e68c81e09cbd4c3a8492f202f4ccbd0b75
MridulKatara <kataramridul2@gmail.com>
2024-01-02 00:07:04 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'sukhdevkatara.github.io',
+  // base: 'sukhdevkatara.github.io',
   plugins: [react()],
 })
